3f8f45a8d73cbaa0852a8d30ffd18d24b3f89678
donyaesolo <35720283+donyaesolo@users.noreply.github.com>
2019-12-23 10:22:06 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ var leftImg = document.getElementsByClassName('contain-img')[0]
 var rightImg = document.getElementsByClassName('contain-img')[1]
 var soft = document.getElementsByClassName('left')[0]
 var boy = document.getElementsByClassName('right')[0]
+var cli = document.getElementsByClassName('click')[0]
+var ck = document.getElementsByClassName('ck')[0]
+var menu = document.getElementsByClassName('menu')[0]
 
 window.onload = function () {
   document.addEventListener('click', clickEffect)
@@ -28,21 +31,22 @@ window.onload = function () {
   leftImg.style.marginLeft = '0%'
   rightImg.style.marginRight = '200%'
   setTimeout(function () {
-    document.getElementsByClassName('click')[0].style.transition = ' ease 1s'
-    document.getElementsByClassName('ck')[0].style.transition = 'ease 1s'
-    document.getElementsByClassName('click')[0].style.opacity = '1'
-    document.getElementsByClassName('ck')[0].style.opacity = '1'
+    cli.style.transition = ' ease 1s'
+    ck.style.transition = 'ease 1s'
+    cli.style.opacity = '1'
+    ck.style.opacity = '1'
+    menu.style.transition = 'ease 1s'
+    menu.style.opacity = '1'
     document.addEventListener('click', playMusic)
   }, 4000)
 }
-
 function playMusic () {
   var windowSize = window.matchMedia('(max-width: 490px)')
   console.log(windowSize)
   audio()
   document.removeEventListener('click', playMusic)
-  document.getElementsByClassName('click')[0].style.opacity = '0'
-  document.getElementsByClassName('ck')[0].style.opacity = '0'
+  cli.style.opacity = '0'
+  ck.style.opacity = '0'
   setInterval(function () {
     if (windowSize.matches) {
       boy.style.transition = 'ease 1s'
@@ -69,13 +73,12 @@ function playMusic () {
   song.innerHTML = 'Song Title: Bossa No Se'
   artist.innerHTML = 'By: Cuco'
 }
-
 var currentSong = 0
-
 function audio () {
   $('#audio')[0].src = $('#playlist li a')[0]
   $('#audio')[0].play()
   $('#playlist li a').click(function (e) {
+     check()
      e.preventDefault()
      $('#audio')[0].src = this
      $('#audio')[0].play()
@@ -83,18 +86,17 @@ function audio () {
       currentSong = $(this).parent().index()
       $(this).parent().addClass('current-song')
   })
-
   $('#audio')[0].addEventListener('ended', function () {
       check()
      currentSong++
-      if (currentSong == $('#playlist li a').length)
+      if (currentSong === $('#playlist li a').length)
           currentSong = 0
       $('#playlist li').removeClass('current-song')
-      $("#playlist li:eq("+currentSong+")").addClass('current-song')
+      $('#playlist li:eq(' + currentSong + ')').addClass('current-song')
       $('#audio')[0].src = $('#playlist li a')[currentSong].href
       $('#audio')[0].play()
   })
-  if (songPlayer.src === 'https://donyaesolo.github.io/softboy/audio/Bossa%20No%20Se.mp3') {
+  if (currentSong === 0) {
     bossa = setInterval(function () {
       if (songPlayer.currentTime >= 14.5) {
         leftCol.style.background = 'white'
@@ -117,7 +119,41 @@ function audio () {
   }
 }
 var bossa
+function cliche () {
+  currentSong = 0
+}
+function boys () {
+  currentSong = 1
+}
+function chinese () {
+  currentSong = 2
+}
+function bubble () {
+  currentSong = 3
+}
+function falling () {
+  currentSong = 4
+}
+function dontKnow () {
+  currentSong = 5
+}
+function october () {
+  currentSong = 6
+}
+function ultimently () {
+  currentSong = 7
+}
+function garden () {
+  currentSong = 8
+}
+function fountain () {
+  currentSong = 9
+}
+function bossaNo () {
+  currentSong = 10
+}
 function check () {
+  console.log(songPlayer.src)
   if (currentSong === 0) {
     clearInterval(bossa)
     soft.classList.remove('bossaText')
@@ -149,20 +185,23 @@ function check () {
       }
     }, 50)
   } else if (currentSong === 1) {
+    clearInterval(bossa)
     song.innerHTML = 'Song Title: Boys Will Be Bugs'
     artist.innerHTML = 'By: cavetown'
-    holder[0].style.left = '80%'
+    holder[0].style.left = '75%'
     setTimeout(function () {
       holder[0].style.left = '100%'
     }, 2000)
   } else if (currentSong === 2) {
+    clearInterval(bossa)
     song.innerHTML = 'Song Title: Chinese New Year'
     artist.innerHTML = 'By: SALES'
-    holder[0].style.left = '80%'
+    holder[0].style.left = '75%'
     setTimeout(function () {
       holder[0].style.left = '100%'
     }, 2000)
   } else if (currentSong === 3) {
+    clearInterval(bossa)
     song.innerHTML = 'Song Title: Bubble Gum'
     artist.innerHTML = 'By: Chairo'
     holder[0].style.left = '80%'
@@ -170,6 +209,7 @@ function check () {
       holder[0].style.left = '100%'
     }, 2000)
   } else if (currentSong === 4) {
+    clearInterval(bossa)
     song.innerHTML = 'Song Title: Falling For U'
     artist.innerHTML = 'By: Peachy!'
     holder[0].style.left = '80%'
@@ -177,20 +217,24 @@ function check () {
       holder[0].style.left = '100%'
     }, 2000)
   } else if (currentSong === 5) {
+    clearInterval(bossa)
     song.innerHTML = 'Song Title: I Don' + "'" + 't Know My Name'
     artist.innerHTML = 'By: Grace VanderWaal'
-    holder[0].style.left = '80%'
+    holder[0].style.left = '75%'
     setTimeout(function () {
       holder[0].style.left = '100%'
     }, 2000)
   } else if (currentSong === 6) {
+    clearInterval(bossa)
     song.innerHTML = 'Song Title: We Fell In Love In October'
+    artist.style.marginTop = '-2%'
     artist.innerHTML = 'By: girl in red'
     holder[0].style.left = '80%'
     setTimeout(function () {
       holder[0].style.left = '100%'
     }, 2000)
   } else if (currentSong === 7) {
+    clearInterval(bossa)
     song.innerHTML = 'Song Title: Ultimently'
     artist.innerHTML = 'By: Khai Dreams'
     holder[0].style.left = '80%'
@@ -198,27 +242,30 @@ function check () {
       holder[0].style.left = '100%'
     }, 2000)
   } else if (currentSong === 8) {
+    clearInterval(bossa)
     song.innerHTML = 'Song Title: Walk But In A Garden'
     artist.innerHTML = 'By: Llusion'
-    holder[0].style.left = '80%'
+    holder[0].style.left = '75%'
     setTimeout(function () {
       holder[0].style.left = '100%'
     }, 2000)
   } else if (currentSong === 9) {
+    clearInterval(bossa)
     song.innerHTML = 'Song Title: Water Fountain'
     artist.innerHTML = 'By: Alec Benjamin'
-    holder[0].style.left = '80%'
+    holder[0].style.left = '78%'
     setTimeout(function () {
       holder[0].style.left = '100%'
     }, 2000)
   } else if (currentSong === 10) {
+    clearInterval(bossa)
     song.innerHTML = 'Song Title: Bossa No Se'
     artist.innerHTML = 'By: Cuco'
     holder[0].style.left = '80%'
     setTimeout(function () {
       holder[0].style.left = '100%'
     }, 2000)
-    setTimeout(function () {
+    if (songPlayer.currentTime >= 15.1) {
       leftCol.style.background = 'white'
       rightCol.style.background = '#EBFAFF'
       leftLine.style.borderColor = '#EBFAFF'
@@ -229,15 +276,59 @@ function check () {
       boy.style.color = 'white'
       leftImg.src = 'images/soft3.png'
       rightImg.src = 'images/soft4.jpg'
-    }, 15100)
-    setTimeout(function () {
+    } if (songPlayer.currentTime >= 43) {
       soft.classList.add('bossaText')
       leftLine.classList.add('bossaLine')
       rightCol.classList.add('bossa')
-    }, 43000)
+    }
   }
 }
-
+function playlistMenu () {
+  var windowSize = window.matchMedia('(max-width: 490px)')
+  var iphone8 = window.matchMedia('(max-height: 740px)')
+  var menu = document.getElementsByClassName('menu')[0]
+  var popup = document.getElementsByClassName('playlist')[0]
+  if (windowSize.matches) {
+    if (iphone8.matches) {
+      console.log('aa')
+      menu.style.marginTop = '80%'
+      popup.style.transition = 'ease 1s'
+      popup.style.bottom = '55%'
+      menu.removeEventListener('click', playlistMenu)
+      menu.addEventListener('click', hidePlaylistMenu)
+    } else {
+      menu.style.marginTop = '85%'
+      popup.style.transition = 'ease 1s'
+      popup.style.bottom = '60%'
+      menu.removeEventListener('click', playlistMenu)
+      menu.addEventListener('click', hidePlaylistMenu)
+    }
+  } else {
+    menu.style.marginLeft = '25%'
+    popup.style.transition = 'ease 1s'
+    popup.style.right = '75%'
+    menu.removeEventListener('click', playlistMenu)
+    menu.addEventListener('click', hidePlaylistMenu)
+  }
+}
+function hidePlaylistMenu () {
+  var windowSize = window.matchMedia('(max-width: 490px)')
+  var menu = document.getElementsByClassName('menu')[0]
+  var popup = document.getElementsByClassName('playlist')[0]
+  if (windowSize.matches) {
+    menu.style.marginTop = '0%'
+    popup.style.transition = 'ease 1s'
+    popup.style.bottom = '100%'
+    menu.addEventListener('click', playlistMenu)
+    menu.removeEventListener('click', hidePlaylistMenu)
+  } else {
+    menu.style.marginLeft = '0%'
+    popup.style.transition = 'ease 1s'
+    popup.style.right = '100%'
+    menu.addEventListener('click', playlistMenu)
+    menu.removeEventListener('click', hidePlaylistMenu)
+  }
+}
 function clickEffect (e) {
   var d = document.createElement('div')
   d.className = 'clickEffect'
